c904abd0a3f755b77f81bb52efa14509fb91e2dd
Aung Thiha <mr.aungthiha@gmail.com>
2019-11-16 23:22:43 +0700

    implement refresh photos

--- photo/src/main/java/thiha/aung/boilerplate/photo/domain/interactors/RefreshPhotos.kt
+++ photo/src/main/java/thiha/aung/boilerplate/photo/domain/interactors/RefreshPhotos.kt
@@ -1,7 +1,26 @@
 package thiha.aung.boilerplate.photo.domain.interactors
 
 import io.reactivex.Completable
+import thiha.aung.boilerplate.core.ext.with
+import thiha.aung.boilerplate.core.scheduler.SchedulerProvider
+import thiha.aung.boilerplate.photo.data.PhotoRepository
 
 interface RefreshPhotos {
     operator fun invoke(): Completable
+}
+
+class RefreshPhotosImpl(
+    private val schedulerProvider: SchedulerProvider,
+    private val photoRepository: PhotoRepository
+)  : RefreshPhotos {
+
+    override fun invoke(): Completable {
+        return photoRepository.run {
+            getRemotePhotos()
+                .with(schedulerProvider)
+                .doOnSuccess(::savePhotos)
+                .ignoreElement()
+        }
+    }
+
 }
