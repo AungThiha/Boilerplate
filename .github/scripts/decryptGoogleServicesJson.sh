556441077ab8e34885cbf2feb5839fa4ea86c9f4
Aung Thiha <mr.aungthiha@gmail.com>
2019-12-29 18:09:13 +0700

    test

--- .github/scripts/decryptGoogleServicesJson.sh
+++ .github/scripts/decryptGoogleServicesJson.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
-decrypt() {
-	# --batch to prevent interactive command --yes to assume "yes" for questions
-	# Add GOOGLE_SERVICE_JSON_SECRET value to github secret for github actions
-	# On your local machine, add GOOGLE_SERVICE_JSON_SECRET as an env variable
-	gpg --quiet --batch --yes --decrypt \
-	--passphrase="$GOOGLE_SERVICES_JSON_SECRET" \
-	--output "./app/src/$1/google-services.json" \
-	"./app/src/$1/google-services.json.gpg"
-}
+cat ./app/src/dev/google-services.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$GOOGLE_SERVICES_JSON_SECRET" --output "./app/src/dev/google-services.json" "./app/src/dev/google-services.json.gpg"
 
-if [[ "$#" -eq 1 ]]; then
-    decrypt $1
-else
-	decrypt "dev"
-	decrypt "staging"
-	decrypt "prod"
-fi
+#decrypt() {
+#	# --batch to prevent interactive command --yes to assume "yes" for questions
+#	# Add GOOGLE_SERVICE_JSON_SECRET value to github secret for github actions
+#	# On your local machine, add GOOGLE_SERVICE_JSON_SECRET as an env variable
+#	gpg --quiet --batch --yes --decrypt \
+#	--passphrase="$GOOGLE_SERVICES_JSON_SECRET" \
+#	--output "./app/src/$1/google-services.json" \
+#	"./app/src/$1/google-services.json.gpg"
+#}
+#
+#if [[ "$#" -eq 1 ]]; then
+#    decrypt $1
+#else
+#	decrypt "dev"
+#	decrypt "staging"
+#	decrypt "prod"
+#fi
