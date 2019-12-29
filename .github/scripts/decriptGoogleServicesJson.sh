2280256ff4020d29ff33504c8891f000e7a4b860
Aung Thiha <mr.aungthiha@gmail.com>
2019-12-29 17:01:31 +0700

    decrpt google-services.json before running unit tests

---
+++ .github/scripts/decriptGoogleServicesJson.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+decrypt() {
+	# --batch to prevent interactive command --yes to assume "yes" for questions
+	# Add GOOGLE_SERVICE_JSON_SECRET value to github secret for github actions
+	# On your local machine, add GOOGLE_SERVICE_JSON_SECRET as an env variable
+	gpg --quiet --batch --yes --decrypt \
+	--passphrase="$GOOGLE_SERVICE_JSON_SECRET" \
+	--output "./app/src/$1/google-services.json" \
+	"./app/src/$1/google-services.json.gpg"
+}
+
+if [[ "$#" -eq 1 ]]; then
+    decrypt $1
+else
+	decrypt "dev"
+	decrypt "staging"
+	decrypt "prod"
+fi
