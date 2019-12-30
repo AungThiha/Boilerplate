38c44db544d8d8b2eed4763dcf668553cca24f12
Aung Thiha <mr.aungthiha@gmail.com>
2019-12-30 17:22:34 +0700

    use actual script

--- .github/scripts/writeKeystore.sh
+++ .github/scripts/writeKeystore.sh
@@ -2,27 +2,21 @@
 
 echo "Writing keystore.properties"
 echo ${KEYSTORE_PROPERTIES} > keystore.properties
+
+writeKeystore() {
+    echo "Writing $1.keystore"
+    # command inside parenthesis converts  $1 to upper case
+    input="$(echo "$1" | tr '[a-z]' '[A-Z]')_KEYSTORE"
+    output="./keystores/$1.keystore"
+    echo "${!input}" > ${output}
+}
+
 mkdir keystores
 
-echo "Writting dev.keystore"
-input="DEV_KEYSTORE"
-filename="dev"
-output="./keystores/${filename}.keystore"
-echo "${!input}" > ${output}
-
-#writeKeystore() {
-#    echo "Writing $1.keystore"
-#    # command inside parenthesis converts  $1 to upper case
-#    input="$(echo "$1" | tr '[a-z]' '[A-Z]')_KEYSTORE"
-#    output="$1.keystore"
-#    echo "${input} input"
-#    echo "${!input}" > ${output}
-#}
-#
-#if [ "$#" -eq 1 ]; then # double brackets do not work in ubuntu 18.04
-#    writeKeystore $1
-#else
-#	writeKeystore "dev"
-#	writeKeystore "staging"
-#	writeKeystore "prod"
-#fi
+if [ "$#" -eq 1 ]; then # double brackets do not work in ubuntu 18.04
+    writeKeystore $1
+else
+	writeKeystore "dev"
+	writeKeystore "staging"
+	writeKeystore "prod"
+fi
