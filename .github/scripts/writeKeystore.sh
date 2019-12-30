0ee1ac16e7c8cf2f3999289b9acdc6bb2f566600
Aung Thiha <mr.aungthiha@gmail.com>
2019-12-30 17:06:34 +0700

    echo input

--- .github/scripts/writeKeystore.sh
+++ .github/scripts/writeKeystore.sh
@@ -8,6 +8,7 @@ writeKeystore() {
     # command inside parenthesis converts  $1 to upper case
     input="$(echo "$1" | tr '[a-z]' '[A-Z]')_KEYSTORE"
     output="$1.keystore"
+    echo "${input} input"
     echo "${!input}" > ${output}
 }
 
