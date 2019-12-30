aa2c6664f525948640357e332ee5f5688608bbdd
Aung Thiha <mr.aungthiha@gmail.com>
2019-12-30 17:09:23 +0700

    some test code

--- .github/scripts/writeKeystore.sh
+++ .github/scripts/writeKeystore.sh
@@ -3,19 +3,23 @@
 echo "Writing keystore.properties"
 echo ${KEYSTORE_PROPERTIES} > keystore.properties
 
-writeKeystore() {
-    echo "Writing $1.keystore"
-    # command inside parenthesis converts  $1 to upper case
-    input="$(echo "$1" | tr '[a-z]' '[A-Z]')_KEYSTORE"
-    output="$1.keystore"
-    echo "${input} input"
-    echo "${!input}" > ${output}
-}
+echo "Writting dev.keystore"
+output="dev.keystore"
+echo $DEV_KEYSTORE > ${output}
 
-if [ "$#" -eq 1 ]; then # double brackets do not work in ubuntu 18.04
-    writeKeystore $1
-else
-	writeKeystore "dev"
-	writeKeystore "staging"
-	writeKeystore "prod"
-fi
+#writeKeystore() {
+#    echo "Writing $1.keystore"
+#    # command inside parenthesis converts  $1 to upper case
+#    input="$(echo "$1" | tr '[a-z]' '[A-Z]')_KEYSTORE"
+#    output="$1.keystore"
+#    echo "${input} input"
+#    echo "${!input}" > ${output}
+#}
+#
+#if [ "$#" -eq 1 ]; then # double brackets do not work in ubuntu 18.04
+#    writeKeystore $1
+#else
+#	writeKeystore "dev"
+#	writeKeystore "staging"
+#	writeKeystore "prod"
+#fi
